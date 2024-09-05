9c8bdba52fd762b56c5e47a7b22345b0dd1e424f
Giovanni <gpetruc@gmail.com>
2024-09-05 14:10:02 +0200

    Add also W->3pi in runScoutingPhase2PuppiTkEm_cfg.py

--- L1TriggerScouting/Phase2/test/runScoutingPhase2PuppiTkEm_cfg.py
+++ L1TriggerScouting/Phase2/test/runScoutingPhase2PuppiTkEm_cfg.py
@@ -164,6 +164,13 @@ process.scPhase2TkEmRawToDigiStruct = cms.EDProducer('ScPhase2TkEmRawToDigi',
   fedIDs = cms.vuint32(*tkEmStreamIDs),
 )
 
+process.w3piStruct = cms.EDProducer("ScPhase2PuppiW3PiDemo",
+    src = cms.InputTag("scPhase2PuppiRawToDigiStruct"),
+    runCandidate = cms.bool(False),
+    runStruct = cms.bool(True),
+    runSOA = cms.bool(False)
+)
+
 process.wdsgStruct = cms.EDProducer("ScPhase2PuppiWDsGammaDemo",
     src = cms.InputTag("scPhase2PuppiRawToDigiStruct"),
     src2 = cms.InputTag("scPhase2TkEmRawToDigiStruct"),
@@ -188,7 +195,13 @@ process.scPhase2TkEleStructToTable = cms.EDProducer("ScTkEleToOrbitFlatTable",
     doc = cms.string("L1TkEle candidates"),
 )
 
-process.p_all = cms.Path(
+process.p_w3pi = cms.Path(
+  process.scPhase2PuppiRawToDigiStruct
+  +process.w3piStruct
+  +process.scPhase2PuppiStructToTable
+)
+
+process.p_wdsg = cms.Path(
   process.scPhase2PuppiRawToDigiStruct+
   process.scPhase2TkEmRawToDigiStruct+
   process.scPhase2PuppiStructToTable+
@@ -207,6 +220,17 @@ process.scPhase2PuppiStructNanoAll = cms.OutputModule("OrbitNanoAODOutputModule"
     compressionAlgorithm = cms.untracked.string("LZ4"),
 )
 
+process.scPhase2PuppiStructNanoW3pi = cms.OutputModule("OrbitNanoAODOutputModule",
+    fileName = cms.untracked.string(options.outFile.replace(".root","")+".w3pi.root"),
+    selectedBx = cms.InputTag("w3piStruct","selectedBx"),
+    outputCommands = cms.untracked.vstring("drop *",
+        "keep l1ScoutingRun3OrbitFlatTable_scPhase2PuppiStructToTable_*_*",
+        "keep l1ScoutingRun3OrbitFlatTable_w3piStruct_*_*"
+        ),
+    compressionLevel = cms.untracked.int32(4),
+    compressionAlgorithm = cms.untracked.string("LZ4"),
+)
+
 process.scPhase2PuppiStructNanoWDsg = cms.OutputModule("OrbitNanoAODOutputModule",
     fileName = cms.untracked.string(options.outFile.replace(".root","")+".wdsg.root"),
     selectedBx = cms.InputTag("wdsgStruct","selectedBx"),
@@ -221,9 +245,10 @@ process.scPhase2PuppiStructNanoWDsg = cms.OutputModule("OrbitNanoAODOutputModule
 )
 
 process.o_all = cms.EndPath( process.scPhase2PuppiStructNanoAll )
-process.o_WDsg = cms.EndPath( process.scPhase2PuppiStructNanoWDsg )
+process.o_w3pi = cms.EndPath( process.scPhase2PuppiStructNanoW3pi )
+process.o_wdsg = cms.EndPath( process.scPhase2PuppiStructNanoWDsg )
 
-sched = [ process.p_all ]
+sched = [ process.p_w3pi, process.p_wdsg ]
 if options.outMode != "none":
   sched.append(getattr(process, "o_"+options.outMode))
 process.schedule = cms.Schedule(*sched)
