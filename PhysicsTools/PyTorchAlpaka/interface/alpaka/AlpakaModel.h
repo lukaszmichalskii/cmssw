89d0db56f7d1d78e997f3a5ff225e29ceeb1e28f
Lukasz Michalski <lukasz.artur.michalski@cern.ch>
2025-09-09 14:15:33 +0200

    [PyTorchAlpaka] add alpaka config for direct and heterogeneous inference, initial support for host <-> device memcpy for AMD ROCm/HIP

---
+++ PhysicsTools/PyTorchAlpaka/interface/alpaka/AlpakaModel.h
@@ -0,0 +1,64 @@
+#ifndef PhysicsTools_PyTorchAlpaka_interface_alpaka_AlpakaModel_h
+#define PhysicsTools_PyTorchAlpaka_interface_alpaka_AlpakaModel_h
+
+#include "alpaka/alpaka.hpp"
+
+#include "HeterogeneousCore/AlpakaInterface/interface/config.h"
+#include "PhysicsTools/PyTorch/interface/Model.h"
+#include "PhysicsTools/PyTorchAlpaka/interface/Converter.h"
+#include "PhysicsTools/PyTorchAlpaka/interface/alpaka/Config.h"
+
+namespace ALPAKA_ACCELERATOR_NAMESPACE::torch {
+
+  class AlpakaModel : public cms::torch::Model {
+  public:
+    using cms::torch::Model::forward;
+    using cms::torch::Model::to;
+
+    // Default model loads to CPU memory space, to be moved to accelerator memory space later in async fashion.
+    explicit AlpakaModel(const std::string &model_path)
+        : cms::torch::Model(model_path) {}
+    
+    // Loads model to alpaka accelerator specified memory space.
+    template <typename T>
+      requires ::alpaka::isDevice<T> || ::alpaka::isQueue<T>
+    explicit AlpakaModel(const std::string &model_path, const T &acc_mem)
+        : cms::torch::Model(model_path, getDevice(acc_mem)) {}
+
+    // Forward pass (inference) of model with SoA metadata input/output.
+    // Allows to run inference directly using SoA portable objects/collections without excessive copies and conversions.
+    // Refer: PhysicsTools/PyTorch/interface/Converter.h for details about wrapping memory layouts.
+    // TODO: add support for multi-output models (without temporary mem copy)
+    template <typename InMemLayout, typename OutMemLayout>
+    void forward(const cms::torch::alpakatools::ModelMetadata<InMemLayout, OutMemLayout> &metadata) {
+      auto input_tensor = cms::torch::alpakatools::Converter::convert_input(metadata, device_);
+      cms::torch::alpakatools::Converter::convert_output(metadata, device_) = model_.forward(input_tensor).toTensor();
+    }
+
+    // Move model to specified device memory space. Async load (in default stream if not overridden by the caller)
+    template <typename T>
+      requires ::alpaka::isDevice<T> || ::alpaka::isQueue<T>
+    void to(const T &acc_mem) {
+      if constexpr (std::is_same_v<::alpaka::Dev<T>, ::alpaka::DevCpu>) {
+        this->Model::to(getDevice(acc_mem));
+        return;
+      }
+#ifdef ALPAKA_ACC_GPU_HIP_ENABLED
+      // ROCm/HIP not yet directly supported → fallback to CPU inference
+      this->Model::to(getDevice(acc_mem));
+      return;
+#else
+      // CUDA → keep async execution
+      this->Model::to(getDevice(acc_mem), true);
+#endif  // ALPAKA_ACC_GPU_HIP_ENABLED 
+    }
+    
+    // Overload for Queue to simplify the interface for the common case of async execution.
+    void to(const Queue &queue) {
+      this->AlpakaModel::to(::alpaka::getDev(queue));
+    }
+  };
+
+}  // namespace ALPAKA_ACCELERATOR_NAMESPACE::torch
+
+#endif  // PhysicsTools_PyTorchAlpaka_interface_alpaka_AlpakaModel_h
---
+++ PhysicsTools/PyTorchAlpaka/interface/alpaka/Nvtx.h
@@ -0,0 +1,38 @@
+#ifndef PhysicsTools_PyTorchAlpakaTest_interface_alpaka_Nvtx_h
+#define PhysicsTools_PyTorchAlpakaTest_interface_alpaka_Nvtx_h
+
+#ifdef ALPAKA_ACC_GPU_CUDA_ENABLED
+#include <nvtx3/nvToolsExt.h>
+#endif
+
+namespace ALPAKA_ACCELERATOR_NAMESPACE {
+
+  class Nvtx {
+  public:
+    explicit Nvtx(const char* msg) {
+#ifdef ALPAKA_ACC_GPU_CUDA_ENABLED
+      id_ = nvtxRangeStartA(msg);
+#endif
+    }
+
+    void end() {
+#ifdef ALPAKA_ACC_GPU_CUDA_ENABLED
+      if (active_) {
+        active_ = false;
+        nvtxRangeEnd(id_);
+      }
+#endif
+    }
+
+    ~Nvtx() { end(); }
+
+  private:
+#ifdef ALPAKA_ACC_GPU_CUDA_ENABLED
+    nvtxRangeId_t id_;
+    bool active_ = true;
+#endif
+  };
+
+}  // namespace ALPAKA_ACCELERATOR_NAMESPACE
+
+#endif  // PhysicsTools_PyTorchAlpakaTest_interface_alpaka_Nvtx_h
